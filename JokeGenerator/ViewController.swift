7f847413c6408969a48a3c2fda9d7d4e018b6159
MilhamAt <M477ashiddiq@gmail.com>
2020-12-19 17:22:54 +0700

    first urlsession without design pattern and use storyboard

--- JokeGenerator/ViewController.swift
+++ JokeGenerator/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        fetchData()
+    }
+    
+    @IBAction func answerPressed(_ sender: UIButton) {
+        
+    }
+    func fetchData(){
+       guard let url = URL(string: "https://sv443.net/jokeapi/v2/joke/Any")
+       else {return}
+        
+        URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else {return}
+            print(data)
+            
+            do{
+                let getData = try JSONDecoder().decode(JokeGenerator.self, from: data)
+                
+                let questionUpdate = getData.setup
+                let answerUpdate = getData.delivery
+                
+                DispatchQueue.main.async {
+                    self.statusLabel.text = "\(questionUpdate)"
+                    self.answerLabel.text = answerUpdate
+                }
+                
+            }catch let err {
+                print("error",err)
+            }
+        }.resume()
+    }
+}
 
+struct JokeGenerator: Codable {
+    let setup: String
+    let delivery: String
 }
 
